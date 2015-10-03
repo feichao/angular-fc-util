883795d5269a371caf43c6b20a1b9ef326bf5a70
FeichaoWan <len.may@foxmail.com>
2015-10-03 18:23:43 +0800

    add fcdocclick

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # angular util
-angular util
+
+> angular util, include 2 components, fcclear && fcdocclick.
+
+> angular 指令工具库，目前包含两个工具指令，一个是fcclear，用于清除输入框或者选择框的内容，一个是fcdocclick，可用于关闭弹出组件的组件。
+
+### [wait for demo](http://blog.0xfc.cn/) ###
+
+> demo/index.html
+
+**dependencies**
+> angular-fc-util depends on angular 1.3.5 as bower.json says.
+
+**install**
+> `bower install angular-fc-util --save`
+
+**how to use**
+
+> in you index.html include angular-util.js and angular-util.css
+> `app.module('your angular app name', 'fc.util')`
+
+**use like this**
+
+ 1. **fcClear**
+```html
+<input ng-model="name" fcclear fc-class="clear-name" fc-icon-class="clear-name-icon">
+```
+    Input element with fcclear will be like this: 
+![clear][1]
+    
+    The clear button will be added, and click it will clear the content of input element.
+
+    How to do this? Very easy! I warp the input element with a div element, so the final html is:
+
+```html
+<div class="fc-clear-content clear-name">
+  <input ng-model="name">
+  <div class="fc-clear-icon clear-name-icon" ng-click="name=''" ng-show="name"> 
+    <svg>...</svg> 
+    <div class="fc-clear-tooltip">Clear</div>
+  </div>
+</div>
+```
+    
+    Read the final html, you should understand fc-class and fc-icon-class.
+ 2. **fcDocClick**
+    
+```html
+<div fc-doc-click="dosomething()">
+  <input ng-model="name" placeholder="Name" fcclear fc-class="clear-name" fc-icon-class="clear-name-icon">
+</div>
+```
+
+    That you click the area outside the div with fc-doc-click will trigger dosomething().
+
+
+  [1]: http://7xl1b4.com1.z0.glb.clouddn.com/util-clear.png
--- src/angular-util.js
+++ src/angular-util.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict';
 
-  var app = angular.module('fc.clear', []);
+  var app = angular.module('fc.util', []);
 
   app.directive('fcclear', FcClear);
 
@@ -14,29 +14,29 @@
       },
       controllerAs: 'vm',
       template: '',
-      compile: Compile
+      compile: ClearCompile
     };
   }
 
-  function Compile(tEle, tAttrs, transcludeFn) {
+  function ClearCompile(tEle, tAttrs, transcludeFn) {
     var tplDiv = angular.element('<div class="fc-clear-content"></div>');
     var tplClear = angular.element([
       '<div class="fc-clear-icon">',
       ' <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18">',
       '   <path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm5 13.59L15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41 8.41 7 12 10.59 15.59 7 17 8.41 13.41 12 17 15.59z"></path>',
       ' </svg>',
+      ' <div class="fc-clear-tooltip">Clear</div>',
       '</div>'
     ].join(''));
 
-    var model = tEle.find('input').attr('ng-model') || tEle.attr('ng-model');
+    var model = tEle.attr('ng-model');
     tplClear.attr('ng-click', model + '=""');
     tplClear.attr('ng-show', model);
 
     var tplEle = tEle.clone();
     var contentClass = tplEle.attr('fc-class') || '';
     var iconClass = tplEle.attr('fc-icon-class') || '';
-    var flex = tplEle.attr('flex') || '';
-    tplEle.removeAttr('flex');
+    
     tplEle.removeAttr('fcclear');
     tplEle.removeAttr('fc-class');
     tplEle.removeAttr('fc-icon-class');
@@ -44,9 +44,8 @@
     tplDiv.append(tplEle);
     tplDiv.append(tplClear);
 
-    flex && tplDiv.attr('flex', flex);
     tplDiv.addClass(contentClass);
-    tplDiv.find('ng-md-icon').addClass(iconClass);
+    tplClear.addClass(iconClass);
 
     tEle.after(tplDiv);
 
@@ -55,4 +54,34 @@
     };
   }
 
+  app.directive('fcDocClick', FcDocClick);
+
+  var contains = function(container, contained) {
+    var node = contained.parentNode;
+    while (node !== null && node !== container) {
+      node = node.parentNode;
+    }
+    return node !== null;
+  };
+
+  function FcDocClick(doc) {
+    return {
+      restrict: 'A',
+      link: function($scope, $element, $attributes) {
+        var fun, onDocClick;
+        fun = $attributes.fcDocClick;
+        onDocClick = function(event) {
+          if (!contains($element[0], event.target)) {
+            $scope.$apply(fun);
+          }
+        };
+        doc.on('click', onDocClick);
+        $element.on('$destroy', function() {
+          doc.off('click', onDocClick);
+        });
+      }
+    };
+  }
+
+  FcDocClick.$inject = ['$document'];
 })();
