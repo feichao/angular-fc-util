96ea569ce4bc26da8dfcffeb8cd953444412bf5e
FeichaoWan <len.may@foxmail.com>
2015-10-03 13:26:49 +0800

    add clear

---
+++ src/angular-util.js
@@ -0,0 +1,58 @@
+(function() {
+  'use strict';
+
+  var app = angular.module('fc.clear', []);
+
+  app.directive('fcclear', FcClear);
+
+  function FcClear() {
+    return {
+      restrict: 'A',
+      scope: {
+        fcClass: '@',
+        fcIconClass: '@'
+      },
+      controllerAs: 'vm',
+      template: '',
+      compile: Compile
+    };
+  }
+
+  function Compile(tEle, tAttrs, transcludeFn) {
+    var tplDiv = angular.element('<div class="fc-clear-content"></div>');
+    var tplClear = angular.element([
+      '<div class="fc-clear-icon">',
+      ' <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18">',
+      '   <path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm5 13.59L15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41 8.41 7 12 10.59 15.59 7 17 8.41 13.41 12 17 15.59z"></path>',
+      ' </svg>',
+      '</div>'
+    ].join(''));
+
+    var model = tEle.find('input').attr('ng-model') || tEle.attr('ng-model');
+    tplClear.attr('ng-click', model + '=""');
+    tplClear.attr('ng-show', model);
+
+    var tplEle = tEle.clone();
+    var contentClass = tplEle.attr('fc-class') || '';
+    var iconClass = tplEle.attr('fc-icon-class') || '';
+    var flex = tplEle.attr('flex') || '';
+    tplEle.removeAttr('flex');
+    tplEle.removeAttr('fcclear');
+    tplEle.removeAttr('fc-class');
+    tplEle.removeAttr('fc-icon-class');
+
+    tplDiv.append(tplEle);
+    tplDiv.append(tplClear);
+
+    flex && tplDiv.attr('flex', flex);
+    tplDiv.addClass(contentClass);
+    tplDiv.find('ng-md-icon').addClass(iconClass);
+
+    tEle.after(tplDiv);
+
+    return function(scope, ele, attrs) {
+      ele.remove();
+    };
+  }
+
+})();
